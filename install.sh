1435a3391d5cef5aea8375798f9b1a6a87b787f0
OCHEANIC <87293342+OCHEANIC@users.noreply.github.com>
2022-02-14 20:02:29 +0530

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,15 @@
+##############################
+#      Youtube Streamer      #
+#             By             #
+#          OCHEANIC          #
+#      www.ocheanic.tech     #
+##############################
+
+if [[ $EUID -ne 0 ]]; then
+  echo "* Please run this script with Root privileges (sudo su)" 1>&2
+  exit 1
+printf "Installing Dependencies\n"
+apt-get update
+apt-get install ffmpeg
+printf "Suscessfully installed the dependencies\n"
+printf "You may change the video Url , Music url , Stream key in stream.sh\n"
